a734199c0ecd30f2b00ad79696056160d726bb4e
mydigitalcrown <mydigitalcrown@gmail.com>
2025-11-04 10:48:52 +0530

    fix: add 'use client' directive to footer component for styled-jsx support

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link"
 import Image from "next/image"
 import { Twitter, Linkedin, Mail, Instagram, Facebook, Sparkles, Zap, Phone, HeadphonesIcon, Bot, MessageSquare, Users, Building2 } from "lucide-react"
